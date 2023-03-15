cc292060660204bcf5be4faefbe6d1ef13e6d271
pankaj <pankajthekush@gmail.com>
2023-03-15 14:15:07 +0530

    added network

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # deployment script on cloud
+docker network create xtnet
 pull docker.io/pkumdev/libpostal-flask:v1
 git clone https://github.com/pankajthekush/libpostal-flask.git /home/ubuntu/libpostal-flask
 cd /home/ubuntu/libpostal-flask
